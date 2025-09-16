69e5d4d48ee3947eba00cfc9c1cf536b8ac4dee4
Frames <3078295834@qq.com>
2025-09-16 23:50:04 +0800

    在TRSM类中新增SetMatrixA方法和matrix_A_成员变量，增强了矩阵A的设置功能，提升了操作的灵活性和可用性。

--- include/cuda_op/detail/cuBlas/trsm.hpp
+++ include/cuda_op/detail/cuBlas/trsm.hpp
@@ -2,6 +2,7 @@
 
 #include "cuda_op/abstract/operator.hpp"
 #include "util/status_code.hpp"
+#include "data/tensor.hpp"
 
 namespace cu_op_mem {
 
@@ -15,9 +16,11 @@ class Trsm : public Operator<T> {
         StatusCode Forward(const Tensor<T>& input,Tensor<T>& output) override;
 
         void SetAlpha(T alpha);
+        void SetMatrixA(const Tensor<T>& A);
     private:
         int side_,uplo_,trans_,diag_;
         T alpha_;
+        Tensor<T> matrix_A_;
 };
 
 }
